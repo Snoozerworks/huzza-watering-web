8daec3d94cf023b71a6fd4704cf9403544d388a7
Snoozerworks <snoozerworks@gmail.com>
2017-07-24 18:45:17 +0200

    Updated get and set parameters

--- download.php
+++ download.php
@@ -9,7 +9,7 @@ $pump3_vol = 0; // Pump flow cc/day
 $pump_flow = 2000; // Pump flow capacity in cc/min.
 $run_interval = 4 * 3600; // Interval in seconds between pump activations.
 $tank_volume = 6000; // Tank volume in cc
-                      
+                     
 // Connection intervall in milliseconds for the IoT thing
 $refresh_rate = 2 * 60 * 1000;
 $refresh_rate = 10000;
@@ -27,14 +27,13 @@ echo pack ( 'C', ( int ) CMD\SET );
 echo pack ( 'CN', ( int ) PRM\P1_RQST_VOL, ( int ) $pump1_vol );
 echo pack ( 'CN', ( int ) PRM\P2_RQST_VOL, ( int ) $pump2_vol );
 echo pack ( 'CN', ( int ) PRM\P3_RQST_VOL, ( int ) $pump3_vol );
-
 echo pack ( 'CN', ( int ) PRM\P1_FLOW, ( int ) $pump_flow );
 echo pack ( 'CN', ( int ) PRM\P2_FLOW, ( int ) $pump_flow );
 echo pack ( 'CN', ( int ) PRM\P3_FLOW, ( int ) $pump_flow );
-
 echo pack ( 'CN', ( int ) PRM\TANK_VOL, ( int ) $tank_volume );
 echo pack ( 'CN', ( int ) PRM\RUN_INTERVAL, ( int ) $run_interval );
 echo pack ( 'CN', ( int ) PRM\REFRESH_RATE, ( int ) $refresh_rate );
+
 echo $end;
 
 //
@@ -42,17 +41,15 @@ echo $end;
 //
 
 echo pack ( 'C', ( int ) CMD\GET );
+
 echo pack ( 'C', ( int ) PRM\REFRESH_RATE );
 echo pack ( 'C', ( int ) PRM\RUN_INTERVAL );
-
 echo pack ( 'C', ( int ) PRM\P1_RQST_VOL );
 echo pack ( 'C', ( int ) PRM\P2_RQST_VOL );
 echo pack ( 'C', ( int ) PRM\P3_RQST_VOL );
-
 echo pack ( 'C', ( int ) PRM\P1_PUMPED_VOL );
 echo pack ( 'C', ( int ) PRM\P2_PUMPED_VOL );
 echo pack ( 'C', ( int ) PRM\P3_PUMPED_VOL );
-
 echo pack ( 'C', ( int ) PRM\ADC1 );
 echo pack ( 'C', ( int ) PRM\ADC2 );
 echo pack ( 'C', ( int ) PRM\ADC3 );
